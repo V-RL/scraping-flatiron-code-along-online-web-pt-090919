6beec40af47afbebf05c8971abf5ce5955655fc0
Brandon Furr <brandon.furr123@gmail.com>
2019-10-31 23:56:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,12 +17,12 @@ class Scraper
   end
   
   def get_page
-    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
-    Nokogiri::HTML(html)
+    @html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    Nokogiri::HTML(@html)
   end
   
   def get_courses
-    doc = Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(@html)
     doc.css(".post")
   end
 end
